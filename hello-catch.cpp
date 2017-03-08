0a6adb541cbb19639f1e5d33cc6e463fce7bc09d
KUR <rhdnfka94@gmail.com>
2017-03-08 22:45:23 +0900

    added: catch fail test case

--- hello-catch.cpp
+++ hello-catch.cpp
@@ -1,7 +1,16 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp" 
+
+/*
 #include <iostream>
 
 int main(void) {
 	std::cout << "hello again bitch! \n";
 	system("pause");
 	return 0;
+}
+*/
+
+TEST_CASE("hello catch!", "[fail case!]") {
+	REQUIRE(false);
 }
